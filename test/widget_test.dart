0b2a31417367f097179a7ceb858fcaf0be3363d7
Verwin Polloso <verwinpolloso2@gmail.com>
2026-03-01 13:44:07 +0800

    shitz

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:itec63_midtermact5_polloso_verwin/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const BusinessCardApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
